30fcdc78d1318db8e43d9048094ede72b7e206a6
Joshua Shroades <pokehax@yahoo.com>
2023-12-04 21:19:00 -0500

    Updated Day 3 Part 1
    Removed some unneeded lines left over from debugging.

--- Day 3/part1.py
+++ Day 3/part1.py
@@ -18,26 +18,16 @@ def getPartNumbers(previousLine, currentLine, nextLine):
 			searchStart = max(0,numberStartIndex-1)
 			searchEnd = min(len(currentLine)-1,numberEndIndex+1)
 			valid = False
-			pLine = ""
-			cLine = ""
-			nLine = ""
 			for i in range(searchStart, searchEnd):
 				if(previousLine != ""):
-					pLine += previousLine[i]
 					if(previousLine[i] != "." and not previousLine[i].isdecimal()):
 						valid = True
-				cLine += currentLine[i]
 				if(currentLine[i] != "." and not currentLine[i].isdecimal()):
 					valid = True
 				if(nextLine != ""):
-					nLine += nextLine[i]
 					if(nextLine[i] != "." and not nextLine[i].isdecimal()):
 						valid = True
 			if(valid):
-				# print(pLine)
-				# print(cLine)
-				# print(nLine)
-				# print("#####")
 				returnNumbers.append(currentLine[numberStartIndex:numberEndIndex])
 		charIndex += 1
 	return returnNumbers
